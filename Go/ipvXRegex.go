9930837bf07b446ea45eb1f352e3be70c3310ada
lkeme <1462364497@qq.com>
2022-09-29 12:26:28 +0800

    [add] Go#ipvXRegex

---
+++ Go/ipvXRegex.go
@@ -0,0 +1,34 @@
+package ipvXRegex
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var (
+	ipv6Regex = `^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$`
+	ipv4Regex = `^(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4})`
+)
+
+func isMayOnlyIPv6(host string) bool {
+	match, err := regexp.MatchString(ipv6Regex, host)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	return match
+}
+
+func isMayOnlyIPv4(host string) bool {
+	match, err := regexp.MatchString(ipv4Regex, host)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	return match
+}
+
+func main() {
+	isMayOnlyIPv6("*")
+	isMayOnlyIPv4("*")
+}
--- README.md
+++ README.md
@@ -1,19 +1,34 @@
-# py-common-code
-python3 封装通用代码
+# CodeSnippets
+Code snippets for all your development needs
 
 
-## 功能
+## Function
 
-|文件                |版本           |描述                          |
-|--------------------|---------------|------------------------------|
-|Printer.py          |2019/9/18      |不同场景的格式化打印输出       |
-|RestrictRequests.py |2019/9/18      |多任务协程下的请求限流 基础模型 |
-|AsyncioLoop.py      |2019/9/18      |兼容不同系统的协程逻辑         |
-|Request.py          |2019/9/18      |协程请求与正常请求             |
-|ThreadPool.py       |2019/9/18      |基础线程池                    |
-|MultiThread.py       |2019/9/18      |通用多任务分段多线程执行       |
+```tree
+├─Go
+│      .gitkeep
+│      ipvXRegex.go
+│
+├─Html
+│      .gitkeep
+│
+├─JavaScript
+│      .gitkeep
+│
+├─PHP
+│      .gitkeep
+│
+└─Python
+        .gitkeep
+        AsyncioLoop.py
+        MultiThread.py
+        Printer.py
+        Request.py
+        RestrictRequests.py
+        ThreadPool.py
+```
 
 
-## 关于
-如何优雅的复制代码？节省时间？如何优雅的复制代码？节省时间？  
-("Ctrl+C", "Ctrl+V")，简单轻松搞定需要的功能。
+## Used
+
+("Ctrl+C", "Ctrl+V")
